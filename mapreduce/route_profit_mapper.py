3c91f17b815d721e2a3105a2b36c60d0b40f5472
mmathapelothotse <u22888676@tuks.co.za>
2025-10-08 21:53:14 +0200

    Update route_profit_mapper.py

--- mapreduce/route_profit_mapper.py
+++ mapreduce/route_profit_mapper.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python
+import sys
 
+for line in sys.stdin:
+    try:
+        fields = line.strip().split(',')
+        pickup_loc = fields[1]
+        dropoff_loc = fields[2]
+        total_amount = float(fields[7])
+        trip_distance = float(fields[4])
+        
+        if trip_distance > 0:
+            revenue_per_mile = total_amount / trip_distance
+            route = f"{pickup_loc}-{dropoff_loc}"
+            
+            print(f"{route}\t{total_amount}\t{trip_distance}\t{revenue_per_mile}")
+    except:
+        continue
