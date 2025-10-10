a9cbab9dd7f5930da9cab8b997019d05867af2f0
mmathapelothotse <u22888676@tuks.co.za>
2025-10-10 14:16:30 +0200

    Update convert_parquet_to_csv.py

--- mapreduce/convert_parquet_to_csv.py
+++ mapreduce/convert_parquet_to_csv.py
@@ -1,8 +1,15 @@
 import os
 import pandas as pd
+import subprocess
 
-input_dir = 'data\input'
-output_dir = 'data\output'
+input_dir = 'bigdata-transport-analysis\data\input'
+output_dir = 'bigdata-transport-analysis\data\output'
+hdfs_output_dir = '/nyc_tlc_data/data/output/'
+
+# Hadoop streaming JAR path (update if different)
+HADOOP_STREAMING_JAR = "/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.4.2.jar"
+
+
 
 if not os.path.exists(input_dir):
     print(f"Input directory {input_dir} does not exist. Please check the path.")
@@ -17,8 +24,16 @@ else:
         print(f"Converting {in_path} to {out_path}...")
         try:
             df = pd.read_parquet(in_path)
-            df.to_csv(out_path, index=False)
+            df.head(1000).to_csv(out_path, index=False)
             print(f"Successfully wrote {len(df)} rows.")
+       
+            # Upload CSV to HDFS
+            print(f"Uploading {out_path} to HDFS directory {hdfs_output_dir}...")
+            subprocess.run(["hdfs", "dfs", "-mkdir", "-p", hdfs_output_dir])
+            subprocess.run(["hdfs", "dfs", "-put", "-f", out_path, hdfs_output_dir])
+            print(f"Uploaded {out_fname} to HDFS successfully.")
+
         except Exception as e:
             print(f"Failed to convert {fname}: {e}")
+
 
